206f99fd6d1c2c5d9c4ac37ef273cdb085652a37
Prasun Nandy Pritam <pritamnandy84@yahoo.com>
2022-04-02 23:30:50 +0600

    2nd Apr

--- shop/lib/widgets/product_item.dart
+++ shop/lib/widgets/product_item.dart
@@ -12,7 +12,7 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -28,15 +28,16 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(
-              product.isFavourite! ? Icons.favorite : Icons.favorite_outline,
+          leading: Consumer<Product>(
+            builder: (context, product, child) => IconButton(
+              icon: Icon(
+                product.isFavourite! ? Icons.favorite : Icons.favorite_outline,
+              ),
+              color: Theme.of(context).accentColor,
+              onPressed: () {
+                product.toggleFavouriteStatus();
+              },
             ),
-            color: Theme.of(context).accentColor,
-            onPressed: () {
-              product.toggleFavouriteStatus();
-              print(product.isFavourite);
-            },
           ),
           title: Text(
             product.title,
