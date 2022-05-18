94548666e045b258b6d83f34c3360b8f04ab0ddc
Prasun Nandy Pritam <pritamnandy84@yahoo.com>
2022-05-18 10:57:43 +0600

    18th May

---
+++ food_delivery/lib/widgets/expendable_text.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExpendableText extends StatefulWidget {
+  const ExpendableText({Key? key}) : super(key: key);
+
+  @override
+  State<ExpendableText> createState() => _ExpendableTextState();
+}
+
+class _ExpendableTextState extends State<ExpendableText> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
